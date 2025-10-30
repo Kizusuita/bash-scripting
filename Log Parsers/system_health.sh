47be3a0c30d8a2ba969c4b6575d28fb988c6f557
Kizusuita <laffattheshadows@gmail.com>
2025-10-30 11:23:51 -0400

    Add files via upload

--- Log Parsers/system_health.sh
+++ Log Parsers/system_health.sh
@@ -15,11 +15,26 @@ fi
 
 case "$1" in
     "-c"|"--cpu")
-        echo -e "Load Average: $(grep "load average" /var/log/syslog | tail -10)\n"
-        echo -e "OOM Events:\n $(grep -i "Out of memory" /var/log/syslog && grep -i "Killed process" /var/log/syslog)\n"
-        echo -e "Swap Usage:\n $(grep -i "swap" /var/log/syslog | grep -ie "used\|exhausted")\n"
+        echo -e "Load Average: $(uptime | awk '{print $NF}')\n"
+        echo -e "OOM Events:\n $((dmesg | grep -i "Out of memory") && (dmesg | grep -i "Killed process"))\n"
+        echo -e "Swap Usage:\n"
+
+        declare -a mem_usage
+        declare -a lines
+        mapfile -t mem_usage < <(smemstat -m | sort -n -k 2 | awk '{print $2}' | grep -E '[[:digit:]]')
+        mapfile -t lines < <(smemstat -m | sort -n -k 2 | awk '{print $1, $2, $7}' | grep -v "Note")
+
+        for (( i=0; i<${#mem_usage[@]}; i++ )); do
+            count=$(bc -l <<< 'scale=3, mem_usage[i]')
+            if (( $(echo $count '> 0' | bc -l) )); then
+                printf '%s' lines[i]
+            fi
+        done
+        echo
+
         echo -e "Hung Tasks:\n $(grep -i "hung task" /var/log/kern.log)\n"
         exit;;
+
     "-d"|"--disk")
         echo -e "I/O Errors: $(grep -iE "I/O error|bad block" /var/log/syslog)\n"
         if ! grep -i "No space left on device" /var/log/syslog; then
@@ -29,27 +44,32 @@ case "$1" in
         fi
         echo -e "Mount Errors: $(grep -iE "fs error|mount|umount" /var/log/syslog)\n"
         exit;;
+
     "-n"|"--network")
         echo -e "Interfaces up/down:\n $(grep -iE "link is up|down" /var/log/syslog)\n"
         echo -e "DHCP/IP Conflict Issues:\n $(grep -i "dhclient" /var/log/syslog | grep -i "error\fail")"
         echo -e "Network Timeouts:\n $(grep -iE "timeout|unreachable|connection refused" /var/log/syslog)\n"
         exit;;
+
     "-s"|"--services")
         echo -e "Failed Systemd Services:\n $(grep -i "failed to start" /var/log/syslog)\n"
         echo -e "Services Restarted:\n $(grep -i "service" /var/log/syslog | grep -i "restart")\n"
         #If you're using journalctl uncomment next line, it replaces previous :)
         #echo -e "Services Restarted:\n $(journalctl -p err -b | head -10)\n"
         exit;;
+
     "-k"|"--kernel")
         echo -e "Kernel panic:\n $(grep -i "kernel panic" /var/log/kern.log)\n"
         echo -e "Hardware Errors:\n $(grep -iE "Machine check|hardware error|thermal|overheat" /var/log/kern.log)"
         echo -e "Power Throttling:\n $(grep -i "throttle" /var/log/syslog)"
         exit;;
+
     "-b"|"--boot")
         echo -e "Recent Boots:\n $(grep "systemd" /var/log/syslog | grep "starting version")\n"
         echo -e "Shutdowns: $(grep -iE "shutdown|poweroff" /var/log/syslog)"
         echo -e "Boot Time Analysis:\n $(systemd-analyze blame | head)"
         exit;;
+
 esac
 
 echo "Section unavailable, did you mistype?"
