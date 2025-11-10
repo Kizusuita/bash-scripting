e358ee2c928e02d398fd59b4f5b597e25e0fe043
Kizusuita <laffattheshadows@gmail.com>
2025-11-10 13:40:35 -0500

    Add files via upload

--- Log Parsers/system_health.sh
+++ Log Parsers/system_health.sh
@@ -4,6 +4,10 @@
 usage: ./system_health.sh "check_section"
 
 Program to check various system health, to help diagnose issues if needed or periodic checking for prevention.
+Because of the hard coded /var/logs, this script only works on Red Hat based systems. If you'd like, you can change /var/log/messages
+to /var/log/syslog for Debian and it'll *probably* work... but no promises.
+
+The -s/--system command uses specfically systemd commands. If you do not use systemd on your system... it's not going to work.
 Program Information
 
 if [[ -z $# ]]; then
@@ -12,7 +16,9 @@ if [[ -z $# ]]; then
     exit 1
 fi
 
-system_type=$(grep "ID=" /etc/os-release)
+system_type=$(grep -E "^ID=" /etc/os-release | sed -E 's/^ID\=//g')
+
+echo -e "Currently Running: $system_type\n-------------------\n"
 
 case "$1" in
     "-c"|"--cpu")
@@ -22,7 +28,7 @@ case "$1" in
 
         declare -a mem_usage
         declare -a lines
-        mapfile -t mem_usage < <(smemstat -m | sort -n -k 2 | awk '{print $2}' | grep -E '[[:digit:]]') #figure this out later, no rh
+        mapfile -t mem_usage < <(smemstat -m | sort -n -k 2 | awk '{print $2}' | grep -E '[[:digit:]]')
         mapfile -t lines < <(smemstat -m | sort -n -k 2 | awk '{print $1, $2, $7}' | grep -v "Note")
 
         for (( i=0; i<${#mem_usage[@]}; i++ )); do
@@ -43,39 +49,52 @@ case "$1" in
         exit;;
 
     "-d"|"--disk")
-        echo -e "I/O Errors:\n $(grep -i "I/O error" /var/log/messages)\n"
+        echo -e "I/O Errors:\n $(dmesg | grep -i "I/O error")"
+        echo -e "Drive Size: $(lsblk | grep -ie 'sda*' | awk '{print $4}' | head -n 1)\n"
+        echo -e "Available Space on /home: $(df -h | grep "/home" | awk '{print $4}')\n"
 
-        if ! grep -i "No space left on device" /var/log/syslog; then
-            echo "Disk full?: No"
-        else
-            echo "Disk full?: *YES* Remedy IMMEDIATELY"
+        mount_error=$(grep -iE "fs error|mount|umount" /var/log/messages)
+
+        if [[ -z $mount_error ]]; then
+            echo "No Mount Errors"
+            exit
         fi
-        echo -e "Mount Errors: $(grep -iE "fs error|mount|umount" /var/log/syslog)\n"
+
+        echo -e "Mount Errors: $mount_error\n"
         exit;;
 
     "-n"|"--network")
-        echo -e "Interfaces up/down:\n $(grep -iE "link is up|down" /var/log/syslog)\n"
-        echo -e "DHCP/IP Conflict Issues:\n $(grep -i "dhclient" /var/log/syslog | grep -i "error\fail")"
-        echo -e "Network Timeouts:\n $(grep -iE "timeout|unreachable|connection refused" /var/log/syslog)\n"
+        mapfile -t intface < <(ifconfig | awk '{print $1}' | grep -E "*:" 2> /dev/null | grep -v "lo")
+        mapfile -t ipaddr < <(ifconfig | grep "inet" | grep -vE "127.0.0.1|::" | awk '{print $2, $4}')
+
+        echo -e "Interfaces:~#\n------------- "
+
+        for (( i=0; i<${#intface[@]}; i++ )); do
+            echo "${intface[i]} ${ipaddr[i]}"
+        done
+
+        echo -e "\nDHCP/IP Conflict Issues:\n $(grep -i "dhclient" /var/log/messages | grep -iE "error|fail")"
+        echo -e "Network Timeouts:\n $(grep -iE "timeout|unreachable|connection refused" /var/log/messages)\n"
         exit;;
 
     "-s"|"--services")
-        echo -e "Failed Systemd Services:\n $(grep -i "failed to start" /var/log/syslog)\n"
-        echo -e "Services Restarted:\n $(grep -i "service" /var/log/syslog | grep -i "restart")\n"
-        #If you're using journalctl uncomment next line, it replaces previous :)
-        #echo -e "Services Restarted:\n $(journalctl -p err -b | head -10)\n"
+        echo -e "Services Started: $(systemctl list-units --type=service --state=active | tail -n 1)\n"
+        echo -e "Failed Systemd Services: $(systemctl list-units --type=service --state=failed | tail -n 1)\n"
         exit;;
 
     "-k"|"--kernel")
-        echo -e "Kernel panic:\n $(grep -i "kernel panic" /var/log/kern.log)\n"
-        echo -e "Hardware Errors:\n $(grep -iE "Machine check|hardware error|thermal|overheat" /var/log/kern.log)"
-        echo -e "Power Throttling:\n $(grep -i "throttle" /var/log/syslog)"
+        echo -e "kdump active?: $(systemctl status kdump | grep "Active:" | sed 's/^[[:space:]]*//g')\n\n----------------------\n"
+        echo -e "Hardware Errors:\n$(grep -iE "Machine check|hardware error|thermal|overheat" /var/log/messages)\n"
+        echo -e "Unknown Hardware:~#\n----------------------\n$(hwinfo --short | awk '/unknown/{flag=1;next} flag' | sed 's/^[[:space:]]*//g')\n"
+        echo -e "Power Throttling:\n $(grep -i "throttle" /var/log/messages)"
         exit;;
 
     "-b"|"--boot")
-        echo -e "Recent Boots:\n $(grep "systemd" /var/log/syslog | grep "starting version")\n"
-        echo -e "Shutdowns: $(grep -iE "shutdown|poweroff" /var/log/syslog)"
-        echo -e "Boot Time Analysis:\n $(systemd-analyze blame | head)"
+        time=$(last reboot shutdown | tail -n 1 | cut -d " " -f3-7)
+
+        echo -e "$(journalctl -b | head -n 2 | awk '{print $6, $7, $8, $36}')\n"
+        echo -e "User Shutdowns (from $time): $(last reboot shutdown | wc -l)\n"
+        echo -e "Time Intensive Boot Processes:~#\n--------------------------------\n$(systemd-analyze blame | head -n 5)"
         exit;;
 
 esac
